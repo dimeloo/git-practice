0ee4e528b7d143ec8fea8172dfd1a15a77f0c241
Mike Ortiz <dimelo@Mikes-MacBook-Air.local>
2022-05-30 21:03:02 -0400

    git instructions

---
+++ git.js
@@ -0,0 +1,11 @@
+// git commands
+
+// git init = create resources to track changes
+// git add . = adds all of the files in the project to be tracked
+// git commit -m "began app setup" = similar to saving but only locally
+// git push origin master = if you have a 'remote' host setup, this pushes the latest version to the remote host
+
+// general workflow
+// git add . = adds changes
+// git commit - m 'some comment' = saves this version
+// git push = uploads this version
