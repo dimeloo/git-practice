3b359d4e51fc3fc54deafe73fa13270b7dac071b
Mike Ortiz <dimelo@Mikes-MacBook-Air.local>
2022-06-01 20:12:18 -0400

    fizzBuzz.js

---
+++ fizzBuzz.js
@@ -0,0 +1,14 @@
+for (i = 1; i <= 30; i++) {
+  const multipleThree = i % 3;
+  const multipleFive = i % 5;
+
+  if (multipleFive === 0 && multipleThree === 0) {
+    console.log(i, "FizzBuzz");
+  } else if (multipleThree === 0) {
+    console.log(i, "Fizz");
+  } else if (multipleFive === 0) {
+    console.log(i, "Buzz");
+  } else console.log(i, "----");
+}
+
+console.log(i, "FizzBuzz");
